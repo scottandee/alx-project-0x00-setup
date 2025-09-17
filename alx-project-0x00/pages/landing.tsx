3df4a0e07d085a9bdc1e77b9aca7a96ad1f3b981
Andee Olayinkascott <andeeolayinkascott@gmail.com>
2025-09-17 15:17:22 +0100

    Rename buttons

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -9,18 +9,18 @@ const Landing: React.FC = () => {
       <Card />
       <Card />
 
-      <Button title="Get in" styles="small rounded-sm bg-blue-300"/>
-      <Button title="Get in 1" styles="medium rounded-sm bg-blue-400"/>
-      <Button title="Get in 2" styles="large rounded-sm bg-blue-500"/>
-      <Button title="Get in 3" styles="small rounded-md bg-blue-300"/>
-      <Button title="Get in 4" styles="medium rounded-md bg-blue-400"/>
-      <Button title="Get in 5" styles="large rounded-md bg-blue-500"/>
-      <Button title="Get in 6" styles="small rounded-full bg-blue-300"/>
-      <Button title="Get in 4" styles="medium rounded-full bg-blue-400"/>
-      <Button title="Get in 6" styles="large rounded-full bg-blue-500"/>
-      <Button title="Get in 6" styles="small rounded-lg bg-blue-300"/>
-      <Button title="Get in 4" styles="medium rounded-lg bg-blue-400"/>
-      <Button title="Get in 6" styles="large rounded-lg bg-blue-500"/>
+      <Button title="Small" styles="small rounded-sm bg-blue-300"/>
+      <Button title="Medium" styles="medium rounded-sm bg-blue-400"/>
+      <Button title="Large" styles="large rounded-sm bg-blue-500"/>
+      <Button title="Small" styles="small rounded-md bg-blue-300"/>
+      <Button title="Medium" styles="medium rounded-md bg-blue-400"/>
+      <Button title="Large" styles="large rounded-md bg-blue-500"/>
+      <Button title="Small" styles="small rounded-full bg-blue-300"/>
+      <Button title="Medium" styles="medium rounded-full bg-blue-400"/>
+      <Button title="Large" styles="large rounded-full bg-blue-500"/>
+      <Button title="Small" styles="small rounded-lg bg-blue-300"/>
+      <Button title="Medium" styles="medium rounded-lg bg-blue-400"/>
+      <Button title="Large" styles="large rounded-lg bg-blue-500"/>
     </div>
   )
 }
